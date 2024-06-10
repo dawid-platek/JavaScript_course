b3578b71bb3ae7c20d349b76470ccfc062576d56
Dev-Data-Net <dev.data.net@gmail.com>
2024-06-10 22:44:40 +0200

    Section 1 - data types

--- file.js
+++ file.js
@@ -1,9 +1,14 @@
-/*console.log("Hello World, ");
+console.log("Hello World, ");
 
 console.log("from Visual Studio Code!");
-*/
+
+let boolean1 = true
+let boolean2 = false
+
+let sampleNumber = 568.32;
+console.log(sampleNumber);
 
 {
-//   let userName = "Test2";
-//  console.log(test);
+  let userName = "Test2";
+  console.log(userName);
 }
