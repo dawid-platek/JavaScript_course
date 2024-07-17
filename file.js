e833bc52b01fda63f9c55591ca482d3dabdeedb7
dawid-platek <dawidplatek.eu.pl@outlook.com>
2024-07-17 22:56:55 +0200

    Section 1 - type number

--- file.js
+++ file.js
@@ -1,19 +1,59 @@
-// FUNKCJE - zadanie
+// typ number
 
-function fullName (firstName, lastName) {
-  return firstName + " " + lastName
-}
+let a = 10
+let b = 5
+
+let sum = a + b // 15
+let difference = a - b // 5
+let product = a * b // 50
+let quotient = a / b // 2
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+
+let eventNumber = 4
+let oddNumber = 7
+
+console.log(eventNumber % 2); // reszta z dzelenia = 0
+console.log(oddNumber % 2); // reszta z dzielenia = 1
+
+let square = a ** 2
+console.log(square); // 100
+
+let textualNumber = "123"
+console.log(textualNumber + 2); // 1232
+
+let textualNumber2 = "123"
+console.log(textualNumber2 * 2); //246
+
+let textualNumber3 = "123"
+console.log(parseInt(textualNumber3) + 2); // 125
+
+console.log(a === "10"); // false -> porównywana jest wartość zmiennych ale też TYP zmiennych
+console.log(a == "10"); // true -> porównywana jest tylko wartość zmiennych
+console.log(a !== b); // true
+console.log(a > b); // true
+console.log(a < b); // false
+console.log(a >= b); // true
+console.log(a <= b); // false
+
+// MATH
+
+console.log( Math.floor(5.9));  // zaokrąglanie do najbliższej liczby całkowitej
+console.log(Math.round(5.6)); // zaokrąglanie do liczby całkowitej
+console.log(Math.abs(-5.6)); // wartość bezwzględna
 
 
-const fullName2 = function (firstName, lastName){
-  return firstName + " " + lastName
-}
 
-const fullName3 = (firstName, lastName) => {
-  return firstName + " " + lastName
-}
 
-console.log(fullName("imie", "nazwisko"));
-console.log(fullName2("imie", "nazwisko"));
-console.log(fullName3("imie", "nazwisko"));
+
+
+
+
+
+
+
+
 
